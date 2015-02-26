eff254327cdb09fb00d7b2514384949e59f92188
NajibAdan <najibadan1@gmail.com>
2015-02-26 18:46:56 +0300

    Amicable Numbers
    Challenge A (Amicable numbers)
    Let d(n) be defined as the sum of proper divisors of n (numbers less
    than n which divide evenly into n). If d(a) = b and d(b) = a, where a ≠
    b, then a and b are an amicable pair and each of a and b are called
    amicable numbers.For example, the proper divisors of 220 are 1, 2, 4, 5,
    10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper
    divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
    Evaluate the product of all the amicable numbers under 10000.

---
+++ amicable.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Mohamed Najib Adan
+int main()
+{
+    int num1=220,num2=284,counter=210,sum2;
+    while (counter<10000)
+    {
+        int div=0;
+        int sum1=0;
+        counter++;
+        while (div<=counter/2)
+        {
+            div++;
+            if ( counter % div == 0 )
+              sum1 += div;
+        }
+        div=0;
+        sum2=sum1;
+        sum1=0;
+        while (div<=sum2/2)
+        {
+            div++;
+            if ( sum2 % div == 0 )
+              sum1 += div;
+        }
+        if ( sum1 == counter )
+          {
+              if ( counter == sum2 ) continue;
+              num1 = counter;
+
+              if ( num1 == num2) continue;
+              num2 = sum2;
+
+              printf("%d, %d\n",num1,num2);
+          }
+    }
+    return 0;
+}
